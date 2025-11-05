8448b21ff9840e2168f5ef71138c36dca2792be3
Tanya233535 <233535@edu.fa.ru>
2025-11-05 21:03:03 +0300

    "Создана точка входа приложения IssAirlineApplication (Spring Boot)"

---
+++ src/main/java/com/example/issairline/IssAirlineApplication.java
@@ -0,0 +1,12 @@
+package com.example.issairline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssAirlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssAirlineApplication.class, args);
+    }
+}
